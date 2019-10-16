ed450bd1cb5a0a33ddfd87a59dbc74bb80636552
Joris <joris.lambrechts@kuleuven.be>
2019-10-16 12:48:18 +0200

    don't import empty nodes + display fix

--- modules/OaiPmhHarvester/src/Job/HarvestJob.php
+++ modules/OaiPmhHarvester/src/Job/HarvestJob.php
@@ -100,8 +100,10 @@ class HarvestJob extends AbstractJob
             $toInsert = [];$toUpdate = [];$ids= []; $update_id='';
             foreach ($records->record as $record) {
                 $pre_item = $this->{$method}($record, $args['collection_id'],$args['resource_template']);
-                $this->itemExists($pre_item);
+                if($pre_item):
+                  $this->itemExists($pre_item);
                   $toInsert[] = $pre_item;
+                endif;  
             }
 
             if($toInsert):
@@ -282,6 +284,10 @@ class HarvestJob extends AbstractJob
             ->metadata
             ->children('oai_dcterms',true)
             ->children('dcterms',true);
+
+        if(!$dcMetadata):
+          return false;
+        endif;
 
         $elementTexts = [];
         $media = [];
